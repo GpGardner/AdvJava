e660264b318f14adb08c089045f6127be53822c5
George Gardner <gpgardner@yahoo.com>
2020-10-11 00:53:34 -0400

    more testing

--- Library/src/Main.java
+++ Library/src/Main.java
@@ -7,9 +7,25 @@ public class Main {
 
 		System.out.println("\n"+libraryOne.getAddress()+ " " +libraryOne.getHours());
 		System.out.println("\n"+libraryTwo.getAddress()+ " " +libraryTwo.getHours());
+		Book bookOne = new Book("Harry Potter", 500);
+		Book bookTwo = new Book("Harry Potter2", 400);
+		Book bookThree = new Book("Harry Potter3", 200);
+		Book bookFour = new Book("Harry Potter4", 600);
+		Book bookFive = new Book("Harry Potter5", 800);
+		
 
-		System.out.println(libraryOne.addBooks(new Book("Harry Potter", 500)));
-		System.out.println(libraryOne.getBooks());
+		System.out.println(libraryOne.addBook(bookOne));
+		System.out.println(libraryOne.addBook(bookTwo));
+		System.out.println(libraryOne.addBook(bookThree));
+		System.out.println(libraryOne.addBook(bookFour));
+		System.out.println(libraryOne.addBook(bookFive));
+		libraryOne.getBooks();
+		System.out.println(libraryOne.rentBook(bookOne.getId()));
+		System.out.println(libraryOne.rentBook(bookTwo.getId()));
+		System.out.println(libraryOne.rentBook(bookOne.getId()));
+		libraryOne.getBooks();
+
+				
 
 	}
 
