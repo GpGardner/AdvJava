2a3efb6db75dc7ed7af24b991c51ca15778b9194
George Gardner <gpgardner@yahoo.com>
2020-10-11 00:06:37 -0400

    mostly finished book

--- Library/src/Book.java
+++ Library/src/Book.java
@@ -1,3 +1,4 @@
+
 /**
  * Book
  */
@@ -16,8 +17,54 @@ public class Book {
 		this.title = title;
 		this.pages = pages;
 		this.id = UUID.randomUUID();
-		this.rented = false;
-		
+		setRented(false);
+
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getPages() {
+		return this.pages;
+	}
+
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+
+	public boolean getRented() {
+		return this.rented;
+	}
+
+	public void setRented(boolean rented) {
+		this.rented = rented;
+	}
+
+	public UUID getId() {
+		return this.id;
+	}
+
+	// public void setId(UUID id) {
+	// this.id = id;
+	// }
+
+	@Override
+	public String toString() {
+		String book;
+		if (this.rented == true) {
+			book = String.format("\nTitle: " + getTitle() + "\nPages: " + getPages() + "\nRental Status: Currently Rented\n");
+		} else {
+			book = String
+					.format("\nTitle: " + getTitle() + "\nPages: " + getPages() + "\nRental Status: Currently Available\n");
+
+		}
+
+		return book;
 	}
 
 }
