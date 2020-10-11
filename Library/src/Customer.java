7d8bcdddef9d9c3552bb064be2d685c3a26d00fb
George Gardner <gpgardner@yahoo.com>
2020-10-11 11:20:38 -0400

    created a customer class

---
+++ Library/src/Customer.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Customer {
+
+	private String name;
+	public ArrayList<Book> rentedBooks = new ArrayList<Book>();
+
+
+	public Customer(String name){
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void getRentedBooks() {
+		if (rentedBooks.size() > 0) {
+			System.out.printf("\n%s is currently renting: \n", name);
+			rentedBooks.forEach((book) -> System.out.println(book.getTitle()));
+		} else {
+			System.out.printf("\n%s, currently doesn't have any books rented\n", name);
+		}
+	}
+
+	public void rentBook(Book book, Library library){
+		library.rentBook(book.getId(), this);
+		System.out.printf("%s: has been successfully rented", book.getTitle());
+	}
+
+
+}
