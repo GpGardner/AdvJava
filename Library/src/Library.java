a0180c9161402c7890cf8623132384d905a276de
George Gardner <gpgardner@yahoo.com>
2020-10-11 11:22:27 -0400

    Adjusting logic to hold rented status

--- Library/src/Library.java
+++ Library/src/Library.java
@@ -5,6 +5,7 @@ public class Library {
 
 	
 	private ArrayList<Book> books = new ArrayList<Book>();
+	private ArrayList<Book> currentlyAvailableBooks = new ArrayList<Book>();
 	private String address;
 	private String hours;
 	
@@ -17,28 +18,27 @@ public class Library {
 		if (books.size() > 0) {
 			System.out.println("\nHere is our current selection: \n");
 			books.forEach((book) -> {
-				if(!book.getRented()){
-					System.out.println(book.getTitle());
-				}
+				if(currentlyAvailableBooks.contains(book)) System.out.println(book.getTitle());
 			});
 		} else {
 			System.out.println("No books available");
 		}
-		
 	}
 
 	public String addBook(Book book) {
 		books.add(book);
+		currentlyAvailableBooks.add(book);
 		final String recipt = String.format("\n%s\nHas been successfully added\nCurrent Stock: %s", book.toString(), books.size());
 		return recipt;
 	}
 
-	public String rentBook(UUID rentalBook) {
+	public String rentBook(UUID rentalBook, Customer customer) {
 		
 		for (int i = 0; i < books.size(); i++) {
 			Book book = books.get(i);
-			if(book.getId() == rentalBook && !book.getRented()){
-				book.setRented(true);
+			if(book.getId() == rentalBook){
+				customer.rentedBooks.add(book);
+				currentlyAvailableBooks.remove(book);
 				final String recipt = String.format("\n%s\nHas been successfully rented!", book.toString());
 				return recipt;
 			}
