04be559ca107b9a03dd8920a8122720f2fa3d8e4
George Gardner <gpgardner@yahoo.com>
2020-10-11 00:07:00 -0400

    Currently testing classes

--- Library/src/Main.java
+++ Library/src/Main.java
@@ -2,6 +2,15 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		Library libraryOne = new Library("123 Main Street", "Monday through Friday 9a to 5pm");
+		Library libraryTwo = new Library("456 Orchard Way", "Monday through Friday 9a to 5pm");
+
+		System.out.println("\n"+libraryOne.getAddress()+ " " +libraryOne.getHours());
+		System.out.println("\n"+libraryTwo.getAddress()+ " " +libraryTwo.getHours());
+
+		System.out.println(libraryOne.addBooks(new Book("Harry Potter", 500)));
+		System.out.println(libraryOne.getBooks());
+
 	}
 
 }
