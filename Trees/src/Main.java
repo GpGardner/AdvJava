c695921c2a39ef0393af699d3194ede55eea6ba4
George Gardner <gpgardner@yahoo.com>
2020-10-25 20:48:51 -0400

    Main method

---
+++ Trees/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+	
+
+	public static void main(String[] args) {
+		BinaryTree tree = new BinaryTree();
+
+		tree.addNode(2, "f");
+		tree.addNode(4, "e");
+		tree.addNode(5, "c");
+		tree.addNode(3, "b");
+		tree.addNode(10, "d");
+		tree.addNode(1, "a");
+
+		tree.preOrderTraverseTree(tree.root);
+	}
+}
